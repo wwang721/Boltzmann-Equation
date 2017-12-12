576a795b24bae2367a4e08e6b130844181d8b6e9
Wang Wei <34364491+NoNo721@users.noreply.github.com>
2017-12-12 23:16:28 +0800

    Update step2.cpp

--- step2.cpp
+++ step2.cpp
@@ -2,81 +2,76 @@
 #include<time.h>
 #include<math.h>
 #include<stdio.h>
-#include"data.h"
-#include"nrutil.h"
+#include<stdlib.h>
 using namespace std;
-void fred2(int n, float a, float b, float t[], float f[], float w[], float (*g)(float), float (*ak)(float, float))
-{
-	void gauleg(float x1, float x2, float x[], float w[], int n);
-	void lubksb(float **a, int n, int *indx, float b[]);
-	void ludcmp(float **a, int n, int *indx, float *d);
-	int i,j,*indx;
-	float d,**omk;
-	indx=ivector(1,n);
-	omk=matrix(1,n,1,n);
-	gauleg(a,b,t,w,n); 
-	for (i=1;i<=n;i++) 
-	{ 
-		for (j=1;j<=n;j++) 
-			omk[i][j]=(float)(i == j)-(*ak)(t[i],t[j])*w[j];
-		f[i]=(*g)(t[i]);
-	}
-	ludcmp(omk,n,indx,&d); 
-	lubksb(omk,n,indx,f);
-	free_matrix(omk,1,n,1,n);
-	free_ivector(indx,1,n);
-}
+
+#include"nrutil.h"
+#include"Boltzmann.h"
+
+#define num 100	//取点数
+
 int main()
 {
 	time_t start,end;
 	start=time(NULL);	//简单计时
-	
-	int i,j;
-	float v;
-	static float s[N],x[N],fak[N][N];
-	
-	for(i=0;i<N;i++)
-		x[i]=(i+1)*d/N;	//x
+	cout<<"程序运行中..."<<endl;
 
-//s1 
-	for(i=0;i<N;i++)
-	{
-		for(v=0.1;v<n;v+=0.1)
-			s[i]+=(c1*exp(-x[i]/(v*tc))+c2*exp(-(d-x[i])/(v*tc)))*0.1;
-		s[i]+=(tc/th)*Nm_0;	
-		printf("%3d. %4.1fμm %14f\n",i+1,x[i]/Mu,s[i]);
-	}	
+	//定义及初始化
+	double *x,*w,*s1,*Nmb;
+	double **fak;
+	
+	x=dvector(1,num);
+	w=dvector(1,num);
+	s1=dvector(1,num);
+	Nmb=dvector(1,num);
+	fak=dmatrix(1,num,1,num);
 
-//fak
-	for(i=0;i<2;i++)
-		for(j=0;j<N;j++)
-		{
-			for(v=0.1;v<n;v+=0.1)
-				fak[i][j]+=(1/(v*tm))*exp(-fabs(x[i]-x[j])/(v*tc))*0.1*d/N;
-			printf("已完成:%.2f%%\n",float(i*N+j+1)/(N*N)*100);
-		}
-/*
-//输出数据
-	FILE *fp;
-	if((fp=fopen("data.txt","w"))==NULL)
+	gauleg(0, distance, x, w, num);	
+	FILE *fp1;
+	if((fp1 = fopen("x.txt","w")) == NULL)
 	{
 		cout<<"Can't open file.\n";
 		exit(1);
 	}
-	for(i=0;i<N;i++)
+	for(int i = 1; i <= num; i++)
+		fprintf(fp1,"%lf\n",x[i]);
+	fclose(fp1);
+	cout<<"<x> done..."<<endl;	//x取点并输出
+
+	s1_Vector(x,s1,num); //s1
+	cout<<"<s1> done..."<<endl;
+
+	input_fak(x,fak,num); //fak
+	cout<<"<fak> done..."<<endl;
+
+	fred2(num,x,w,fak,s1,Nmb);	
+	cout<<"<Nmb> done..."<<endl;
+	FILE *fp4;
+	if((fp4=fopen("Nmb.txt","w"))==NULL)
 	{
-		for(j=0;j<N;j++)
-		{
-			fprintf(fp,"%f",fak[i][j]);
-			if(j==N-1)
-				fprintf(fp,"\n");
-			else
-				fprintf(fp,"\t");
-		}
+		cout<<"Can't open file.\n";
+		exit(1);
 	}
-	fclose(fp);
-*/
+	for(int i=1;i<=num;i++)
+		fprintf(fp4,"%lf\n",Nmb[i]);
+	fclose(fp4);	//求出Nmb并输出
+
+	//释放变量空间
+	free_dvector(x,1,num);
+	free_dvector(w,1,num);
+	free_dvector(s1,1,num);
+	free_dvector(Nmb,1,num);
+    free_dmatrix(fak,1,num,1,num);
+
 	end=time(NULL);
-	cout<<"\n用时:"<<end-start<<"s\n";	//计时结束并输出所用时间
+	cout<<"程序结束！"<<endl;
+	int T,second,minute,hour;
+	T=end-start;
+	second=T%60;
+	minute=T/60;
+	minute=minute%60;
+	hour=minute/60;
+	cout<<"用时:"<<hour<<"h "<<minute<<"min "<<second<<"s.\n";	//计时结束并输出所用时间
+	
 	return 0;
 }
