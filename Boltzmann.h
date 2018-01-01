942101d0afa90ca9ea5de100ed0e41ad4f97a99b
Wang Wei <34364491+NoNo721@users.noreply.github.com>
2018-01-01 21:48:16 +0800

    Update Boltzmann.h

--- Boltzmann.h
+++ Boltzmann.h
@@ -7,20 +7,29 @@
 
 #define Nm_0 0
 
-#define c1 2000
+#define c1 1e9
 #define c2 0
+
+#define sigma 1 //正态分布方差
 
 #define distance 40 //单位：μm
 
 #define Mu 1e-6 //微米数量级
 
-#define intvx 2e9 //对Vx的积分
+#define intvx 2e6 //对Vx的积分
 
-double s1_integrand(double v, double x);
-double fak_integrand(double v, double x, double xp);
+double s1_integrand(double v, double x, double v0);
+double fak_integrand1(double v, double x, double xp, double beta);
+double fak_integrand2(double v, double beta);
+double fak_integrand3(double v, double x, double beta);
+double fak_integrand4(double v, double x, double beta);
 
-void s1_vector(double x[], double s1[], int n); 
-void fak_matrix(double x[], double w[], double **fak, int n);
+void s1_vector(double x[], double s1[], int n, double v0); 
+void fak_matrix(double x[], double w[], double **fak, int n, double beta, int k);
+double vxNm1(double v, int i, double x[], double Nmb[], double v0, double beta);
+double vxNm2(double v, int i, int n, double x[], double Nmb[], double beta);
+void jm_vector(double x[], double jm[], double Nmb[], int n, double v0, double beta, int k);
+//以上是自己定义的
 
 void ludcmp(double **a, int n, int *indx, double *d);
 void lubksb(double **a, int n, int *indx, double b[]);
