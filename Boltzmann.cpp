b36c120a64df9076c3fdc2d88806f8f550e53561
Wang Wei <34364491+NoNo721@users.noreply.github.com>
2018-01-01 21:49:27 +0800

    Update Boltzmann.cpp

--- Boltzmann.cpp
+++ Boltzmann.cpp
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <math.h>
 #include <cmath>
+#include "string.h"
 
 #include "nrutil.h"
 #include "Integ.h" 
@@ -10,45 +11,75 @@
 
 using namespace std;
 
-static double tc=th*tm/(th+tm);
-
-double s1_integrand(double v, double x)
+double s1_integrand(double v, double x, double v0)
 //s1被积函数
 {
-	return (c1*exp(-x*Mu/(v*tc))+c2*exp(-(distance-x)*Mu/(v*tc)));
+	return (c1*exp(-(v-v0)*(v-v0)/(2*sigma*sigma))*exp(-x*Mu/(v*th))+c2*exp(-(distance-x)*Mu/(v*th)));
 }
 
-void s1_vector(double x[], double s1[], int n) 
+void s1_vector(double x[], double s1[], int n, double v0) 
 //s1
 {
+	cout<<"<s1>"<<endl;
 	FILE *fp;
 	if((fp=fopen("s1.txt","w"))==NULL)
 	{
 		cout<<"Can't open file.\n";
 		exit(1);
 	}
-	for(int i=1; i<=n; i++)
+	for(int i=1,p=1; i<=n; i++)
 	{
-		s1[i] = Chebyshev_integ1(Low, Up, s1_integrand, x[i]);
-		s1[i]+=(tc/th)*Nm_0;
-		s1[i]=s1[i]/intvx;
+		s1[i] = Simpson_integ(1e7,Low,Up,s1_integrand,x[i],v0);
+		s1[i]+= Nm_0;
+		s1[i] = s1[i]/intvx;
+		if(i==p*(n/100))
+		{
+			cout<<"已完成:"<<p<<"%"<<endl;
+			p++;
+		}
 		fprintf(fp,"%lf\n",s1[i]);	
 	}	
 	fclose(fp);	//积分并输出
+	cout<<"<s1> done..."<<endl;
 }
 
-double fak_integrand(double v, double x, double xp)
-//fak被积函数
+double fak_integrand1(double v, double x, double xp, double beta)
+//fak被积函数1
 {
-	return ((1/(v*tm))*exp(-fabs(x-xp)*Mu/(v*tc)));
+	return (-(beta/(v*th))*exp(-fabs(x-xp)*Mu/(v*th)));
 }
 
-void fak_matrix(double x[], double w[], double **fak, int n)
+double fak_integrand2(double v, double beta)
+//fak被积函数2
+{
+	return (2*beta);
+}
+
+double fak_integrand3(double v, double x, double beta)
+//fak被积函数3
+{
+	return (-beta*exp(-x*Mu/(v*th)));
+}
+
+double fak_integrand4(double v, double x, double beta)
+//fak被积函数4 
+{
+	return (-beta*exp(-(distance-x)*Mu/(v*th)));
+}
+
+void fak_matrix(double x[], double w[], double **fak, int n, double beta, int k)
 //fak
 {
 	cout<<"<fak>\n";
 	FILE *fp;
-	if((fp=fopen("fak.txt","w"))==NULL)
+	char *name,*order;
+	name=new char;
+	order=new char;
+	strcpy(name,"fak-");
+	sprintf(order,"%d",k+1);
+	name=strcat(name,order);
+	name=strcat(name,".txt");
+	if((fp=fopen(name,"w"))==NULL)
 	{
 		cout<<"Can't open file.\n";
 		exit(1);
@@ -58,7 +89,15 @@ void fak_matrix(double x[], double w[], double **fak, int n)
 	{
 		for(int j=1; j<=n; j++)
 		{
-			fak[i][j] = Chebyshev_integ(Low, Up, fak_integrand, x[i], x[j])*w[j]*Mu/intvx;   
+			fak[i][j] = Chebyshev_integ3(Low, Up, fak_integrand1, x[i], x[j], beta)*w[j]*Mu;   
+			if(i==j)
+				fak[i][j]+=Chebyshev_integ1(Low, Up, fak_integrand2, beta);
+			if(j==1)
+				fak[i][j]+=Chebyshev_integ(Low, Up, fak_integrand3, x[i], beta);
+			if(j==n)
+				fak[i][j]+=Chebyshev_integ(Low, Up, fak_integrand4, x[i], beta);
+			fak[i][j]=fak[i][j]/intvx;
+
 			fprintf(fp,"%lf",fak[i][j]);
 			if(j==n)
 				fprintf(fp,"\n");
@@ -72,6 +111,7 @@ void fak_matrix(double x[], double w[], double **fak, int n)
 		}
 	}
 	fclose(fp);	//以矩阵形式输出
+	cout<<"<fak> done..."<<endl;
 }
 
 #define NRANSI
@@ -180,3 +220,61 @@ void fred2(int n, double t[], double **omk, double g[], double f[])
 	free_ivector(indx,1,n);
 }
 
+double vxNm1(double v, int i, double x[], double Nmb[], double v0, double beta)
+//Nm(x,vx>0)
+{
+	double sum;
+	sum = c1*exp(-(v-v0)*(v-v0)/(2*sigma*sigma))*exp(-x[i]*Mu/(v*th));
+	for(int k=1;k<i;k++)
+		sum	+= beta*(Nmb[k+1]-Nmb[k])*exp(-(x[i]-x[k])*Mu/(v*th));
+	return v*sum;
+}
+
+double vxNm2(double v, int i, int n, double x[], double Nmb[], double beta)
+//Nm(x,vx<0)
+{
+	double sum;
+	sum = c2*exp((distance-x[i])*Mu/(v*th));
+	for(int k=i+1;k<n;k++)
+	{
+		sum -= beta*(Nmb[k+1]-Nmb[k])*exp(-(x[i]-x[k])*Mu/(v*th));
+		
+	}
+	
+	return v*sum;
+}
+
+void jm_vector(double x[], double jm[], double Nmb[], int n, double v0, double beta, int k)
+//jm
+{
+	cout<<"<jm>"<<endl;
+	FILE *fp;
+	char *name,*order;
+	name=new char;
+	order=new char;
+	strcpy(name,"jm-");
+	sprintf(order,"%d",k+1);
+	name=strcat(name,order);
+	name=strcat(name,".txt");
+	if((fp=fopen(name,"w"))==NULL)
+	{
+		cout<<"Can't open file.\n";
+		exit(1);
+	}
+	for(int i=1,p=1; i<=n; i++)
+	{
+		jm[i] = Simpson_integ3(1e6,Low,Up,vxNm1,i,x,Nmb,v0,beta);		//vx>0积分
+		jm[i] += Simpson_integ4(1e7,-1e6,-0.1,vxNm2,i,n,x,Nmb,beta);		//vx<0积分
+		jm[i] = jm[i]/intvx;
+		cout<<jm[i]<<endl;
+
+		if(i==p*(n/100))
+		{
+			cout<<"已完成:"<<p<<"%"<<endl;
+			p++;
+		}	
+		fprintf(fp,"%lf\n",jm[i]);	
+	}	
+	fclose(fp);			//积分并输出
+	cout<<"<jm> done..."<<endl;
+}
